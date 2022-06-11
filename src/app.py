8ab44b17fb7e7f0069ec5f4251f4705ed450bc7b
germanAstudillo1 <g.astudilloc1@gmail.com>
2022-06-11 13:57:54 -0400

    cambios (11/06) - login

--- src/app.py
+++ src/app.py
@@ -1,21 +1,19 @@
 import functools
-from flask import Flask, jsonify, redirect, render_template, request, flash, session, url_for
+from re import X
+from flask import Flask, jsonify, redirect, render_template, request, flash, session, url_for, g
 import cx_Oracle
 from cryptography.fernet import Fernet
-from werkzeug.security import generate_password_hash
-
-import cgi
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-#Models
-from models import ModelUsuario
-from models.entities import Usuario
 
 
 
 app = Flask(__name__)
 key = Fernet.generate_key()
 app.secret_key = "secret_key"
+
+
 
 
 #Conexion bd
@@ -52,7 +50,8 @@ def registrar():
         nombre = new_user['nombre']
         apellido = new_user['apellido']
         email = new_user['email']
-        password = Fernet(key).encrypt(bytes(new_user['password'], 'utf-8'))
+        password = new_user['password']
+        password_hash = generate_password_hash(password)
         error = None
         cur.execute("SELECT id_usuario FROM usuario WHERE email = (:1)", [email])
         if not nombre:
@@ -69,22 +68,99 @@ def registrar():
 
         elif cur.fetchone() is not None:
             error = "Usuario {} se encuentra registrado.".format(email)
+            return redirect('registrar')
 
         if error is None:
-            rows = (nombre, apellido, email, password)
+            rows = (nombre, apellido, email, password_hash)
             statement = "SP_AGREGAR_USUARIO"
             cur.callproc(statement, rows)
             conn.commit()
             conn.close()
             return redirect('login')
-
         cur.close()
+        conn.close()
 
-        flash(error)       
+        print(error)       
 
+        return redirect('registrar')        
+        
     return render_template('registrarU.html')
 
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email-login']
+        password = request.form['password']
+        conn = connection()
+        cur = conn.cursor()
+        error = None
+        cur.execute("SELECT * FROM usuario WHERE email = (:1)", [email])
+        usuario = cur.fetchone()
+        if usuario is None:
+            error = "Usuario y/o contraseña incorrecta1."
+        if not check_password_hash(usuario[4], password):
+            error = "Usuario y/o contraseña incorrecta2."
+
+        if error is None:
+            session.clear()
+            print("HELLO")
+            session['id_usuario'] = usuario[0]
+            session['nombre'] = usuario[1]
+            session['apellido'] = usuario[2]
+            session['email'] = usuario[3]
+            return redirect('/')
+
+
+#        error = None
+#        if usuario is not None:
+#            if password == usuario[4]:
+#                session['id_usuario'] = usuario[0]
+#                session['nombre'] = usuario[1]
+#                session['apellido'] = usuario[2]
+#                session['email'] = usuario[3]
+#                login_user(usuario)
+#                return redirect('/')
+#            else:
+#                error = "Correo y/o contraseña invalida"
+#                print("error aqui")
+#        else:
+#            error = "Correo y/o contraseña invalida"
+#            print("Error en otro aqui")
+        
+        print(error)
+        flash(error)
+        return render_template('login.html')
+        
+
+
+@app.before_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.usuario = None
+    else:
+        conn = connection()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM usuario WHERE id_usuario = (:0)", [user_id])
+        g.usuario = cur.fetchone()
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.usuario is None:
+            return redirect('login')
+
+        return view(**kwargs)     
+
+    return wrapped_view
+
+
 @app.route('/servicios', methods=['GET', 'POST'])
+@login_required
 def servicios():
     if request.method == 'POST':
         conn = connection()
@@ -126,59 +202,42 @@ def servicios():
             conn.commit()
             conn.close()
  
-#        if luzVal == True:        
-#            luz = new_servicio['lblLuz']
+        if luzVal == True:        
+            luz = new_servicio['lblLuz']
+            valorLuz = new_servicio['valorLuz']
+            print(luz, valorLuz)
 
-#        if luzVal == True:        
-#            termografia = new_servicio['lblTermografia']
+            cur = conn.cursor()
+            rows = (luz, valorLuz)
+            statement = "SP_AGREGAR_SERVICIO"
+            cur.callproc(statement, rows)
+            print("Servicio guardado")
+            conn.commit()
+            conn.close()
 
 
-#        termografia = new_servicio['lblTermografia']
-#        valorInstalaciones = new_servicio['valorInstalaciones']
-#        valorMetros = new_servicio['valorMetros']
-#        valorLuz = new_servicio['valorLuz']
-#        valorTermografia = new_servicio['valorTermografia']
+        if termografiaVal == True:        
+            termografia = new_servicio['lblTermografia']
+            valorTermografia = new_servicio['valorTermografia']
+            cur = conn.cursor()
+            rows = (termografia, valorTermografia)
+            statement = "SP_AGREGAR_SERVICIO"
+            cur.callproc(statement, rows)
+            print("Servicio guardado")
+            conn.commit()
+            conn.close()
+
 
         return render_template('servicios.html')
-
-    print('nofunco')
+        
+        
 
 
     return render_template('servicios.html')
 
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-     
-    if request.method == 'POST':
-        email = request.form['email-login']
-        password = request.form['password'] 
-        conn = connection()
-        c = conn.cursor()
-        c.execute("SELECT * FROM usuario WHERE email = (:1)", [email])
-        u = c.fetchone()
-        if u is None:
-            flash('Usuario y/o contraseña invalida')
-
-        print(u[0])
-        
-        user = Usuario.Usuario(id_usario=0, nombre="", apellido="", email=email, password=password)
-        logged_user = ModelUsuario.ModelUsuario.login(con=conn, usuario=user) 
-        
-        if logged_user != None:
-            if logged_user.password:
-                return redirect('/')
-            else:
-                flash('Usuario y/o contraseña invalida')
-        else:
-            flash('Usuario No encontrado')
-
-    else:
-        return render_template('login.html')
-
-
-
+@app.route('/seguimiento')
+def seguimiento():
+    return render_template("seguimiento.html")
 
 
 
